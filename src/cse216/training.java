3dc71a02f7ef93abc8afbd9bd4295180030dcb78
Keneth <Keneth@128.180.134.185>
2014-10-22 19:02:43 -0400

    Blank slate

---
+++ src/cse216/training.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse216;
+
+/**
+ *
+ * @author Keneth
+ */
+public class training {
+    
+}
