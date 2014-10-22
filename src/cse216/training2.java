f86aefc38109dffb3ad14c147c48b449cc55ba11
Keneth <Keneth@128.180.134.185>
2014-10-22 19:41:59 -0400

    Blank slate

---
+++ src/cse216/training2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse216;
+
+/**
+ *
+ * @author Keneth
+ */
+public class training2 {
+    public static void main(String[] args){
+        System.out.println("Seeing if this uploads...");
+    }
+}
