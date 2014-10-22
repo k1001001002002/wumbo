9c99010eb5a1d3da0a945fd8c539470963e86b90
Keneth <Keneth@128.180.134.185>
2014-10-22 19:08:37 -0400

    Blank slate

--- src/cse216/training.java
+++ src/cse216/training.java
@@ -12,5 +12,6 @@ public class training {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("goodbye");
+        System.out.println("Kenneth Wong's push");
     }
 }
