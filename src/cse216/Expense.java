79547eaa8999995bde4dc77f75709ba7d4a0e4a8
Keneth <Keneth@128.180.134.185>
2014-10-23 19:08:36 -0400

    Blank slate

--- src/cse216/Expense.java
+++ src/cse216/Expense.java
@@ -2,4 +2,18 @@ package cse216;
 
 public class Expense {
     
+    public int amount;
+
+    public Expense(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
 }
